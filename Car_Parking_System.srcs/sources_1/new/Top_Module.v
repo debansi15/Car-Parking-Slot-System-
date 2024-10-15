7accf699d2c2088bffaa7d3ad75765a95ce44b73
Debansi15 <debansipatnaik15@gmail.com>
2024-10-15 22:09:11 +0530

    initial commit

---
+++ Car_Parking_System.srcs/sources_1/new/Top_Module.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/15/2024 12:04:02 AM
+// Design Name: 
+// Module Name: Top_Module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_Module(
+input [14:0] cars,
+output [3:0] count,
+output [6:0] seg,
+output [3:0] an
+   );
+   
+ assign an=4'b1110;
+ 
+ Parking_System U0(cars,count);
+ Seven_Segment U1(count,seg);
+ 
+ 
+endmodule
